a96c68f8b707efc22690614c9fca4a71dc474a71
Aditya Undre <104020862+adityaundre@users.noreply.github.com>
2024-01-04 11:44:07 +0530

    Add files via upload

--- Second_largest_in_array.js
+++ Second_largest_in_array.js
@@ -1,16 +1,26 @@
 class Solution{
-    print2largest(arr,n){
+    print2largest(arr,n) 
+    {
         let arr1=arr.sort();
+
         let first_large=arr1[0];
-        for(let i=0;i<arr.length;i++){
-            if(arr[i]>first_large){
+
+        for(let i=0;i<arr.length;i++)
+        {
+            if( arr[i] > first_large)
+            {
                 first_large=arr[i];
             }
             
         }
+
         let sec_large=arr1[0];
-        for(let i=0;i<arr.length;i++){
-            if(arr[i]>sec_large && arr[i]<first_large){
+
+        for(let i=0;i<arr.length;i++)
+        {
+            if(arr[i]>sec_large && arr[i]<first_large)
+            
+            {
                 sec_large=arr[i]
             }
         }
