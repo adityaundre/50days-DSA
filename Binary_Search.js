4851e9d7497728571b0801b1f0c54269ecd0bf81
adityaundre <adityarundre@gmail.com>
2024-01-24 19:55:32 +0530

    BinarySearch

---
+++ Binary_Search.js
@@ -0,0 +1,21 @@
+function searchTarget(N,A,X) {
+    let l=0;
+    let m=A.length;
+
+ 
+    while(l<=m){
+        let mid=Math.floor((l+m)/2);
+        if(A[mid]===X){
+            return mid;
+        }
+        else if(X>A[mid]){
+            l=mid+1;
+        }
+        else {
+            m = mid - 1;
+
+
+    }
+}
+    return -1
+}
